a00391f20ad9b33fb5a040afc03d97dd9b623eab
kamilxcv <kamil.sz00@proton.me>
2026-01-18 18:28:29 +0100

    dodany single project entry

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,10 +1,13 @@
+
 from hello_world import app
 from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
+
 moje_imie = "Kamil"
 msg = "Hello World!"
+
 
 @app.route('/')
 def index():
@@ -13,6 +16,7 @@ def index():
         output = PLAIN
     return get_formatted(msg, moje_imie,
                          output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
